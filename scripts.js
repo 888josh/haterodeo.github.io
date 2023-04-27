dd0e84f918f53a3256d3ff60af6f922c27fa191e
joze <joshua.nghe@stonybrook.edu>
2023-04-27 15:33:45 -0400

    updated functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 const menu = document.getElementById("menu");
 
+window.addEventListener("load", () => {
+    document.querySelector(".loader").classList.add("loader--hidden");
+    document.querySelector(".name").classList.add("name--hidden");
+});
+
+setTimeout(()=>{
+
+    const text = document.getElementById('name');
+
+    text.style.display = 'none';
+
+},1000);
+
 Array.from(document.getElementsByClassName("menu-item"))
     .forEach((item,index) => {
         item.onmouseover = () => {
